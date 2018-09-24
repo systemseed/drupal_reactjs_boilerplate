7f547ce6d8c58dacfe51a580afc2558d498ea80c
spleshka <spleshka@gmail.com>
2018-09-24 10:45:50 +0300

    #5 Finalized tests examples for the boilerplate.

--- tests/api/example/ExampleCest.php
+++ tests/api/example/ExampleCest.php
@@ -3,55 +3,35 @@
 namespace jsonapi;
 
 use Codeception\Util\HttpCode;
+use Drupal\user\Entity\User;
 
 /**
  * Class ExampleCest.
  *
- * Example API test with database connection.
- *
- * @package Jsonapi
+ * Example API tests with database connection.
  */
 class ExampleCest {
 
   /**
-   * Test /jsonapi endpoint for demo purposes.
+   * Test REST authentication for admin.
    *
    * @param \ApiTester $I
    */
-  public function accessJsonapiIndex(\ApiTester $I) {
-    $path_prefix = \Drupal::config('jsonapi_extras.settings')
-      ->get('path_prefix');
-    $I->amGoingTo('Send not authenticated request to the index endpoint.');
-    $I->sendGET("/$path_prefix");
-    $I->expectTo('See an error.');
-    $I->dontSeeResponseCodeIs(HttpCode::OK);
-    $I->amGoingTo('Login as administrator.');
-    $this->loginAsAdministrator($I);
-    $I->amGoingTo('Send request to the index endpoint as an administrator.');
-    $I->sendGET("/$path_prefix");
-    $I->expectTo('Successful response.');
-    $I->seeResponseCodeIs(HttpCode::OK);
-  }
+  public function authenticateAdmin(\ApiTester $I) {
 
-  /**
-   * Internal.
-   *
-   * Test core login endpoint for demo purposes.
-   *
-   * @param \ApiTester $I
-   */
-  private function loginAsAdministrator(\ApiTester $I) {
     $username = 'admin';
     $password = 'admin';
 
-    // Fetch user id from database.
+    // Fetch user id from database (you've got access to the database!).
     $uid = $I->grabFromDatabase(
       'users_field_data',
       'uid',
       ['name' => $username]
     );
+
     // Fail if user is not found.
-    $I->assertNotEmpty($uid, 'Test admin uid should not be empty');
+    $I->assertNotEmpty($uid, 'Admin uid should not be empty');
+
     // Login using Drupal core endpoint. It will set session cookie.
     $I->haveHttpHeader('Content-Type', 'application/json');
     $I->sendPOST(
@@ -61,15 +41,48 @@ class ExampleCest {
         'pass' => $password,
       ]
     );
+
+    $I->expectTo('See successful response.');
     $I->seeResponseCodeIs(HttpCode::OK);
-    // Check if specific key is presented in the response.
+
+    $I->expectTo('Check that specific key exists in the response.');
     $I->seeResponseJsonMatchesJsonPath('$.csrf_token');
-    // Check if specific data is presented in the response.
+
+    $I->expectTo('Check that specific data is presented in the response.');
     $I->seeResponseContainsJson(
       [
         'current_user' => [
           'uid' => $uid,
-          'roles' => ['authenticated', 'administrator']
+          'roles' => ['authenticated']
+        ]
+      ]
+    );
+  }
+
+  /**
+   * Checks that anonymous user can view users info.
+   *
+   * @param \ApiTester $I
+   */
+  public function accessJsonApiUserData(\ApiTester $I) {
+    $path_prefix = \Drupal::config('jsonapi_extras.settings')
+      ->get('path_prefix');
+
+    $I->amGoingTo('Send request to the JSON API user endpoint.');
+    $I->sendGET("/$path_prefix/user/user");
+
+    $I->expectTo('See successful response.');
+    $I->seeResponseCodeIs(HttpCode::OK);
+
+    // Load admin user data from Drupal database and make sure it
+    // exists in the response.
+    $I->expectTo('Find admin user data in the response.');
+    $account = User::load(1);
+    $I->seeResponseContainsJson(
+      [
+        'attributes' => [
+          'uid' => $account->id(),
+          'name' => $account->getAccountName()
         ]
       ]
     );
